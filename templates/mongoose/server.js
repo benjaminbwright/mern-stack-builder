872966ab8594674bfd996659c0af869f164edff2
Benjamin Wright <bwright@trilogyed.com>
2021-06-11 16:09:55 -0500

    Added short circuit to mongoose connect for deployment

--- templates/mongoose/server.js
+++ templates/mongoose/server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // add database connections here
-mongoose.connect(process.env.MONGODB_URI, "mongodb://localhost/exampleDatabase");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/exampleDatabase");
 
 // add middlewares here
 app.use(express.static("client/build"));

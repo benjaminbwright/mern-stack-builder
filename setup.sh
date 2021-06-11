fc667be651cfe0673018a7a2680c54d59ce474db
Benjamin Wright <bwright@trilogyed.com>
2021-06-11 00:59:06 -0500

    setup destroys itself

--- setup.sh
+++ setup.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# install react app
 echo "installing react app"
 npx create-react-app client
 
+# add proxy too the client-side package.json
 echo "adding proxy to client/package.json"
 sed -i '' '/name/a \
 \  "proxy": "http://localhost:4000", \
 ' client/package.json 
+
+# remove .git folder
+rm -rf .git
+
+# destroy setup file
+rm setup.sh
